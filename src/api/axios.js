e138173fcdf8282054a8c759a02b93d414f79d9a
ramiak98 <rami.aboukhalil@outlook.fr>
2025-06-03 12:24:47 +0300

    ayre

--- src/api/axios.js
+++ src/api/axios.js
@@ -9,16 +9,4 @@ const api = axios.create({
         'Content-Type': 'application/json'
     }
 });
-
-// Add response interceptor for better error handling
-api.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response?.status === 405) {
-            console.error('Method not allowed:', error.response.data);
-        }
-        return Promise.reject(error);
-    }
-);
-
 export default api;
